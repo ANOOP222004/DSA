13f6d298c382e64b0eb0a19531f10a5c7fc332a1
Anoop B A <anoop22122004@gmail.com>
2024-09-12 23:36:44 +0530

    Add files via upload

---
+++ Strings/length_of_string.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    char s[] = "welcome";  // String initialization
+    int i;
+
+    // Loop to find the length of the string
+    for (i = 0; s[i] != '\0'; i++) {
+        // The loop will terminate when s[i] becomes '\0' (null character)
+    }
+
+    cout << "Length of the string: " << i << endl;  // Print the length
+    return 0;
+}
